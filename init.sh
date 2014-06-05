7b9de0966c6109348ed90a2b5761fdaeabb4335f
Gus Bonfant <gbonfant@gmail.com>
2014-06-05 12:01:39 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ mv ~/dotfiles/.* -t ~/ -f
 rm -rf ~/dotfiles
 
 git clone git@github.com:gbonfant/sublime_2.git
-sh sublime_2/init.sh
+sh ~/sublime_2/init.sh
 rm -rf sublime_2
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
