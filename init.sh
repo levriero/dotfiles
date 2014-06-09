c81ca3680f0a16f1b2bc558fbd6d2ff35d2f386c
Gus Bonfant <gbonfant@gmail.com>
2014-06-09 02:04:42 +0200

    Updated init.sh to install requirements before applying configuration files.

--- init.sh
+++ init.sh
@@ -4,6 +4,19 @@ cd ~
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install rbenv ruby-build zsh
 
+# Install Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+
+# Create backup and temp folders for Vim
+mkdir ~/.vim/backup
+mkdir ~/.vim/tmp
+
+# Install Vim plugins
+vim +PluginInstall +qall
+
 # Copy dotfiles
 git clone git@github.com:gbonfant/dotfiles.git
 mv ~/dotfiles/.* -t ~/ -f
@@ -14,18 +27,6 @@ git clone git@github.com:gbonfant/sublime_2.git
 sh ~/sublime_2/init.sh
 rm -rf sublime_2
 
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Create backup and temp folders for Vim
-mkdir ~/.vim/backup
-mkdir ~/.vim/tmp
-
-# Install all plugins
-vim +PluginInstall +qall
-
-# Install oh-my-zsh
-curl -L http://install.ohmyz.sh | sh
 
 # Post rbenv installation for zsh
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
