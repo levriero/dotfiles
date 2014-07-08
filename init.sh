984b3ec983dfdd5d97632a8e386cf7eec78ebfdb
Gus Bonfant <gbonfant@me.com>
2014-07-08 10:34:54 +0200

    Install latest version of vim with lua support

--- init.sh
+++ init.sh
@@ -3,7 +3,10 @@ cd ~
 # Install homebrew and some kegs
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install rbenv ruby-build fish ag
+
+# Install latest version of vim and macvim with lua support
 brew install macvim --with-cscope --with-lua --HEAD
+brew install vim --with-lua
 
 # Set fish as default shell
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
