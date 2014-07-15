fefa60a1597b8829234a0f4a3e078648802d3458
Gus Bonfant <gbonfant@me.com>
2014-07-15 22:13:21 +0200

    Better init script

--- init.sh
+++ init.sh
@@ -1,36 +1,66 @@
+#!/bin/sh
+
+echo "      _       _      __ _ _"
+echo "   __| | ___ | |_   / _(_) | ___  ___"
+echo "  / _\` |/ _ \| __|| |_| | |/ _ \/ __|"
+echo " | (_| | (_) | ||  |  _| | |  __/\__ \\"
+echo "  \__,_|\___/ \__| |_| |_|_|\___||___/"
+echo ""
+
 cd ~
+echo "==> Installing..."
 
-# Install homebrew and some kegs
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew install ruby-build fish ag
+# ===================================
+# Homebrew installs
+# ===================================
 
-# Install latest version of rbenv with support for fish
-brew install rbenv --HEAD
+echo "  > Updating homebrew..."
+brew update &> /dev/null
 
-# Install latest version of vim and macvim with lua support
-brew install macvim --with-cscope --with-lua --HEAD
+echo "  > Installing rbenv..."
+if command -v rbenv >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install rbenv --HEAD
+fi
+
+echo "  > Installing ruby-build..."
+if command -v ruby-build >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install ruby-build
+fi
+
+echo "  > Installing the_silver_searcher..."
+if command -v ag >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install the_silver_searcher
+fi
+
+echo "  > Installing fish..."
+if command -v fish >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install fish
+  set EDITOR /usr/local/bin/vim
+fi
+
+echo "  > Installing macvim..."
+if command -v macvim >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install macvim --with-cscope --with-lua --HEAD
+fi
+
+echo "  > Installing vim..."
 brew install vim --with-lua --override-system-vim
 
-# Set fish as default shell
+echo "  > Setting fish as default shell"
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "  > Pulling latest changes"
+git pull &> /dev/null
 
-# Create backup and temp folders for Vim
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/tmp
-
-# Copy git dotfiles into home
-git clone git@github.com:gbonfant/dotfiles.git
-mv ~/dotfiles/.* -t ~/ -f
-rm -rf ~/dotfiles
-
-# Bootstrap Sublime Text 2
-git clone git@github.com:gbonfant/sublime_2.git
-sh ~/sublime_2/init.sh
-rm -rf sublime_2
-
-# Make sure fish uses latest vim as editor
-set EDITOR /usr/local/bin/vim
+echo "==> Done."
