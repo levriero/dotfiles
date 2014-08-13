ba82ed80cbbbdbe032b595f1f2169831901a5c18
Gus Bonfant <gbonfant@me.com>
2014-08-13 14:10:39 +0200

    rename init file

---
+++ script/bootstrap.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "      _       _    __ _ _            "
+echo "     | |     | |  / _(_) |           "
+echo "   __| | ___ | |_| |_ _| | ___  ___  "
+echo "  / _\` |/ _ \| __|  _| | |/ _ \/ __| "
+echo " | (_| | (_) | |_| | | | |  __/\__ \ "
+echo "  \__,_|\___/ \__|_| |_|_|\___||___/ "
+echo "                                     "
+echo "                                     "
+
+
+cd ~/.dotfiles
+echo "==> Installing..."
+
+# ===================================
+# Homebrew installs
+# ===================================
+
+echo "  > Updating homebrew..."
+brew update &> /dev/null
+
+echo "  > Installing rbenv..."
+if command -v rbenv >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install rbenv --HEAD
+fi
+
+echo "  > Installing ruby-build..."
+if command -v ruby-build >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install ruby-build
+fi
+
+echo "  > Installing the_silver_searcher..."
+if command -v ag >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install the_silver_searcher
+fi
+
+echo "  > Installing tree..."
+if command -v tree >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install tree
+fi
+
+echo "  > Installing fish..."
+if command -v fish >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install fish
+
+  echo "  > Setting fish as default shell..."
+  echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+  chsh -s /usr/local/bin/fish
+fi
+
+echo "  > Installing macvim..."
+if command -v mvim >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install macvim --with-cscope --with-lua --HEAD
+fi
+
+echo "  > Installing node..."
+if command -v node >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  brew install node
+fi
+
+echo "  > Installing jshint..."
+if command -v jshint >/dev/null 2>&1; then
+  echo "  > Skipping, already installed"
+else
+  npm install -g jshint
+fi
+
+echo "  > Pulling latest changes..."
+git pull &> /dev/null
+
+echo "  > Copying directories..."
+rsync -ar ~/.dotfiles/.* ~/ --exclude=.git
+rsync -ar ~/.dotfiles/com.*
+
+echo "==> Done."
