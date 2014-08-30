9ae30e9c3cecf8b3898966b1e848326ae1e8577f
Gus Bonfant <gbonfant@me.com>
2014-08-30 13:11:51 +0200

    Added heroku and docker paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,8 +4,9 @@ set fish_greeting
 # PATH
 set default_path /usr/bin /usr/sbin /bin /sbin
 set homebrew /usr/local/bin /usr/local/sbin
+set heroku /usr/local/heroku/bin
 
-set -gx PATH $homebrew $default_path
+set -gx PATH $homebrew $heroku $default_path
 
 # rbenv
 status --is-interactive; and . (rbenv init -|psub)
@@ -13,3 +14,5 @@ status --is-interactive; and . (rbenv init -|psub)
 # Editor
 set -gx EDITOR vim
 
+# Docker daemon
+set -x DOCKER_HOST "tcp://192.168.59.103:2375"
