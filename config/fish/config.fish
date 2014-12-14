fb8a4b3c6cd8735e0caf4ff2caf707b5a8d1c3e6
Gus Bonfant <gbonfant@me.com>
2014-12-14 19:11:38 +0000

    Updated fish config file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,20 +1,12 @@
 # Greeting message
 set fish_greeting
 
-# Set macvim in CLI mode as editor
-set -x EDITOR 'mvim -v'
-set -x GIT_EDITOR 'mvim -v'
-
-# Homebrew
-set homebrew /usr/local/bin
-
 # heroku CLI
 set heroku /usr/local/heroku/bin
 
 # PATH
-set -x PATH $homebrew $heroku $PATH
+set -x PATH $heroku $PATH
 
 # rbenv
-set PATH $HOME/.rbenv/bin $PATH
+set -gx RBENV_ROOT /usr/local/var/rbenv
 . (rbenv init -|psub)
-
