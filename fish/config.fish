3031cde9dde7fa1b7776f13a7ec6d148c3b595f3
Gus Bonfant <gbonfant@me.com>
2014-12-15 13:51:07 +0000

    Updated fish config for heroku

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,5 @@
 # Greeting message
 set fish_greeting
 
-# heroku CLI
-set heroku /usr/local/heroku/bin
-
-# PATH
-set -x PATH $heroku $PATH
-
 # rbenv
 status --is-interactive; and . (rbenv init -|psub)
