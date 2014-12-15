dad272ce2f6cfd7a0b391fb2b9e2c184d82a0af9
Gus Bonfant <gbonfant@me.com>
2014-12-15 00:53:50 +0000

    Moved fish into its own directory to avoid conflicts

--- script/setup.sh
+++ script/setup.sh
@@ -8,7 +8,7 @@ NPM_DEPENDENCIES=(jshint)
 
 create_symlinks() {
   for file in *; do
-    if ! (echo $file | grep -q "script\|README\|config"); then
+    if ! (echo $file | grep -q "script\|README\|fish"); then
       ln -sf $(pwd)/$file $HOME/.$file
     fi
   done
