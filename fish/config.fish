32cb481cccab1781458dbf95790b90b4921de2fb
Gus Bonfant <gbonfant@me.com>
2015-01-23 11:06:35 +0000

    fucking rubymine

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,12 @@ set heroku /usr/local/heroku/bin
 set cdpath $HOME/Projects
 
 set -x EDITOR "vim"
+set -x USE_DEBUGGER true
 set -x CDPATH . ~ $cdpath
 set -x PATH $heroku $PATH
 
 # rbenv
 status --is-interactive; and . (rbenv init -|psub)
+
+alias p=prevd
+alias n=nextd
