a622f3792bb69dba6257dd8c8f66d9893a4321b0
Gus Bonfant <gbonfant@me.com>
2015-01-29 15:56:45 +0000

    Updated fish settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,10 @@
 set fish_greeting
 
-set heroku /usr/local/heroku/bin
 set cdpath $HOME/Projects
 
 set -x EDITOR "vim"
 set -x USE_DEBUGGER true
 set -x CDPATH . ~ $cdpath
-set -x PATH $heroku $PATH
 
 # rbenv
 status --is-interactive; and . (rbenv init -|psub)
---
+++ .config/fish/functions/heroku.fish
@@ -0,0 +1,3 @@
+function heroku
+  /usr/local/bin/heroku
+end
