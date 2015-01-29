2556b3f96bb77aeca488284e2c6dedd4b81532d7
Gus Bonfant <gbonfant@me.com>
2015-01-29 16:39:18 +0000

    added sbin to path for rabbitmq

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set cdpath $HOME/Projects
 set -x EDITOR "vim"
 set -x USE_DEBUGGER true
 set -x CDPATH . ~ $cdpath
+set -x PATH $PATH /usr/local/sbin
 
 # rbenv
 status --is-interactive; and . (rbenv init -|psub)
