86bf704ee90080a4d56daba676bd52d8b5d9659e
Gus Bonfant <gbonfant@me.com>
2015-02-15 20:31:38 +0000

    Smart tmux attach

--- .config/fish/functions/tat.fish
+++ .config/fish/functions/tat.fish
@@ -1,3 +1,15 @@
 function tat
-  tmux attach $argv
+  set session_name (basename "$PWD" | tr . -)
+  set session_exists (tmux list-sessions | sed -E 's/:.*$//' | ag "^"$session_name"\$")
+
+  if test $TMUX
+    if not test $session_exists
+      set TMUX ''
+      tmux new-session -Ad -s $session_name
+    end
+
+    tmux switch-client -t "$session_name"
+  else
+    tmux new-session -As $session_name
+  end
 end
