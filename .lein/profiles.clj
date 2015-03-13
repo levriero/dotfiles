4c78890bef76042ab7714919b7884302238ea6f5
Gus Bonfant <gbonfant@me.com>
2015-03-13 17:38:27 +0000

    Add ultra as user dependency

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+                  [venantius/ultra "0.3.2"]]
+        :ultra {:color-scheme :solarized_dark}}}
