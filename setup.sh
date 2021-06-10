5e35912c8f29b439013b94578c0552977227db46
Gus B <807237+gsbf@users.noreply.github.com>
2021-06-10 15:29:25 +0800

    Add tokyonight color scheme extras

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ ln -sf dotfiles/gitconfig   .gitconfig
 ln -sf dotfiles/tmux.conf   .tmux.conf
 ln -sf dotfiles/vimrc       .vimrc
 
-mkdir -p .config/fish
-ln -sf dotfiles/config/fish .config/fish
+rm -rf .config/fish
+ln -sF ~/dotfiles/config/fish ~/.config/fish
 
 command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew..."; \
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
