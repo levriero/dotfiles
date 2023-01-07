d96c2047ca9b5da5b317c6cc81b5257eaf2d4c51
Gus B <807237+levriero@users.noreply.github.com>
2023-01-07 18:07:20 +0000

    open nvim-tree on startup

--- config/nvim/after/plugin/nvim-tree.lua
+++ config/nvim/after/plugin/nvim-tree.lua
@@ -4,9 +4,10 @@ vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
 require('nvim-tree').setup({
+  open_on_setup = true,
   view = {
     width = 35
-  }
+  },
 })
 
 vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeFindFileToggle)
