a8ae462cfcc534e8bd4f0059306fdd794b9e0c0f
Gus B <807237+levriero@users.noreply.github.com>
2023-01-16 13:59:21 +0000

    add autocommand to remove trailing whitespace

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -1,7 +1,17 @@
-vim.cmd [[
-  augroup toggle_cursorline
-    autocmd!
-    autocmd WinEnter * setlocal cursorline
-    autocmd WinLeave * setlocal nocursorline
-  augroup END
-]]
+-- toggle cusor line
+vim.api.nvim_create_autocmd({ 'WinEnter', 'WinLeave' }, {
+  pattern = { '*' },
+  callback = function (ev)
+    if ev == 'WinEnter' then
+      vim.api.nvim_set_option_value('cursorline', '', 'local')
+    elseif ev == 'WinLeave' then
+      vim.api.nvim_set_option_value('nocursorline', '', 'local')
+    end
+  end
+})
+
+-- remove trailing whitespace
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*' },
+  command = [[%s/\s\+$//e]],
+})
