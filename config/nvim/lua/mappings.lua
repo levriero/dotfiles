03278ebe84a27d7f3fda023ada8dbd1eddb1de7f
Gus B <807237+levriero@users.noreply.github.com>
2023-01-18 11:21:55 +0000

    update mappings

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -20,15 +20,36 @@ keymap.set('n', '<leader>cp', ':let @+ = expand("%:p")<cr>', { silent = true })
 keymap.set('n', '<tab>', '<C-w>w')
 keymap.set('n', '<S-tab>', '<C-w>W')
 
--- Move highlighted blocks up/down
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- Remap for dealing with word wrap
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- delete highlighted word into the void register
+-- Move highlighted blocks up/down
+-- keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+-- keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Delete highlighted word into the void register
 keymap.set('x', '<leader>p', "\"_dP")
 
--- evenly distribute windows
+-- Evenly distribute windows
 keymap.set('n', '<leader>=', ':wincmd =<CR>')
 
--- maximize focus window
+-- Maximize focus window
 keymap.set('n', '<leader>-', ':wincmd _<CR>:wincmd |<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
