37068cb49977223f1b93d6625d0dd62a5eafae08
Gus B <807237+levriero@users.noreply.github.com>
2023-01-18 22:10:30 +0000

    update pure & TERM values

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x EDITOR "nvim"
+
 set -x TERM xterm-256color
+set -x TERMINFO xterm-256color
 
 set -x NVMDIR $XDG_CONFIG_HOME/.nvm
 
@@ -19,3 +21,6 @@ status --is-interactive; and source (rbenv init -|psub)
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "($HOME)/google-cloud-sdk/path.fish.inc" ]; . "($HOME)/google-cloud-sdk/path.fish.inc"; end
+
+# pure-fish/pure
+set pure_enable_single_line_prompt true
