ed7697131cdf3107af88f7b0b5c39b625a6ea27f
Gus B <807237+levriero@users.noreply.github.com>
2023-01-27 19:17:27 +0000

    add caveat resolution for ruby-build

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,18 +9,22 @@ set -x NVMDIR $XDG_CONFIG_HOME/.nvm
 # cargo
 set PATH $HOME/.cargo/bin $PATH
 
-set fish_greeting
-
 # homebrew
 if status --is-interactive
   eval (/opt/homebrew/bin/brew shellenv)
 end
+
+# ruby-build installs a non-Homebrew OpenSSL for each Ruby version installed and these are never upgraded.
+# To link Rubies to Homebrew's OpenSSL 1.1 (which is upgraded) add the following
+set RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "($HOME)/google-cloud-sdk/path.fish.inc" ]; . "($HOME)/google-cloud-sdk/path.fish.inc"; end
+
+set fish_greeting
 
 # pure-fish/pure
 set pure_enable_single_line_prompt true
