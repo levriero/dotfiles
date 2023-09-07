f51a69e55e8d3d0e57a506dbffa10424dfbaeead
Gus B. <807237+levriero@users.noreply.github.com>
2023-09-07 16:09:11 +0100

    fix: (brew) postgres path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,8 +8,9 @@ set -gx EDITOR "nvim"
 set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx GPG_TTY (tty)
 set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH $VOLTA_HOME/bin $PATH
-set -gx PATH $HOME/.cargo/bin $PATH
+
+fish_add_path $VOLTA_HOME/bin
+fish_add_path /opt/homebrew/opt/postgresql@15/bin
 
 if status --is-interactive
 	# exports environment variables needed for brew to work
