c590bb3d67780fad012076e24c7d66bb89d5d7e0
Gus B. <807237+levriero@users.noreply.github.com>
2024-06-16 18:06:54 +0100

    chore: change wezterm tab to "retro style"

--- .wezterm.lua
+++ .wezterm.lua
@@ -27,47 +27,12 @@ config.color_scheme = "Tokyo Night"
 -- If a second tab is created, the tab will be shown.
 config.hide_tab_bar_if_only_one_tab = true
 
--- Native tab bar options
-config.window_frame = {
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
-	font_size = 13.0,
+-- When set to true (the default), the tab bar is rendered in a native style with proportional fonts.
+-- When set to false, the tab bar is rendered using a retro aesthetic using the main terminal font.
+config.use_fancy_tab_bar = false
 
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#1a1b26",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#1a1b26",
-}
-
-config.colors = {
-	tab_bar = {
-		-- The active tab is the one that has focus in the window
-		active_tab = {
-			bg_color = "#1a1b26",
-			fg_color = "white",
-		},
-
-		-- Inactive tabs are the tabs that do not have focus
-		inactive_tab = {
-			bg_color = "#2a2e40",
-			fg_color = "white",
-		},
-
-		-- The new tab button that let you create new tabs
-		new_tab = {
-			bg_color = "#1a1b26",
-			fg_color = "#1a1b26",
-		},
-
-		-- The color of the inactive tab bar edge/divider
-		inactive_tab_edge = "#2a2e40",
-	},
-}
-
--- This function constructs a lua table that corresponds to the internal FontAttributes struct that is used to select a single named font:
+-- This function constructs a lua table that corresponds to the internal FontAttributes struct
+-- that is used to select a single named font:
 config.font = wezterm.font("JetBrainsMono NF", { weight = "Light" })
 config.font_size = 16.0
 
