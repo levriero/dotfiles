b76b994a7a4bb160b219563c3534c6758275a28c
Gus B. <807237+levriero@users.noreply.github.com>
2024-07-17 15:56:41 +0100

    chore: update tokyonight.nvim

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -8,9 +8,6 @@ return {
 		},
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
-
-			-- You can configure highlights by doing something like:
-			-- vim.cmd.hi 'Comment gui=none'
 		end,
 	},
 }
