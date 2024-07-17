0f308856853904d710d2ac2e394fffdfe1731829
Gus B. <807237+levriero@users.noreply.github.com>
2024-07-17 16:37:04 +0100

    feat: add terraform lsp, syntax highlighting, and formatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -16,12 +16,7 @@ return {
 			notify_on_error = true,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
+				terraform = { "terraform_fmt" },
 			},
 		},
 	},
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -131,19 +131,8 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-				--
-
+				gopls = {},
+				marksman = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
@@ -158,9 +147,8 @@ return {
 						},
 					},
 				},
-
-				-- gem install ruby-lsp
 				ruby_lsp = {},
+				terraformls = {},
 			}
 
 			-- Ensure the servers and tools above are installed
